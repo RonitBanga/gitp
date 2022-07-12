6e19d88a3062f5e42829f40dd72ca30acfa79fe9
RonitBanga <bangaronit22@gmail.com>
2022-07-12 10:33:23 +0530

    make

---
+++ Activity_1 - Copy.cpp
@@ -0,0 +1,21 @@
+static bool sortbyfinish(const pair<int,int> &a,const pair<int,int> &b)
+    {
+        return (a.second < b.second);
+    }
+    int Activityselection(int start[], int end[], int n)
+    {
+        // vector<pair<int,int>> v(n);
+        // for(int i=0;i<n;i++){
+        //     v[i].first = start[i];
+        //     v[i].second = end[i];
+        // }
+        // sort(v.begin(), v.end(), sortbyfinish);
+        // int ans = 0;
+        // int e = 0;
+        // for(int i=0;i<n;i++){
+        //     if(v[i].first>=e){
+        //         e= v[i].second;
+        //         ans++;
+        //     }
+        }
+   }
